0216fedf37aab83a8d5b5a5a2116a2bc16ba0533
Poomon001 <57118681+Poomon001@users.noreply.github.com>
2023-01-21 14:41:44 -0800

    checkout

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 dotenv.config();
 
-import { Configuration, OpenAIApi } from 'openai';
+import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI,
+  apiKey: process.env.OPENAI,
 });
+
+console.log("Hello world");
 
 const openai = new OpenAIApi(configuration);
 
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
-const port = process.env.PORT
+const port = process.env.PORT;
 const app = express();
 app.use(cors());
 app.use(express.json());
-
