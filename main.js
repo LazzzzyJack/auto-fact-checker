70dfcc19ce0941dffcf3a0ece95780459bb0d1a2
Poomon001 <57118681+Poomon001@users.noreply.github.com>
2023-01-22 01:09:53 -0800

    merge server into this branch

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const getFactCheck = async (e) => {
   e.preventDefault();
   showSpinner();
   const data = isUrl ? new FormData(urlForm) : new FormData(textForm);
-  const endpoint = isUrl ? 'factcheckurl' : 'factchecktext';
+  const endpoint = isUrl ? "factcheckurl" : "factchecktext";
 
   const response = await fetch("http://localhost:8080/" + endpoint, {
     method: "POST",
@@ -28,7 +28,7 @@ const getFactCheck = async (e) => {
   }
 
   hideSpinner();
-}
+};
 
 function showSpinner() {
   const button = document.querySelector(".submit-button");
@@ -42,19 +42,19 @@ function hideSpinner() {
   button.innerHTML = "Fact Check 🤔";
 }
 
-const pasteUrlButton = document.querySelector('.paste-button');
-const urlTextarea = document.querySelector('.url-textarea');
+const pasteUrlButton = document.querySelector(".paste-button");
+const urlTextarea = document.querySelector(".url-textarea");
 
-pasteUrlButton.addEventListener('click', async () => {
+pasteUrlButton.addEventListener("click", async () => {
   urlTextarea.focus();
   const data = await navigator.clipboard.readText();
   urlTextarea.value = data;
 });
 
-const pasteTextButton = document.querySelector('.paste-article-button');
-const textTextarea = document.querySelector('.text-textarea');
+const pasteTextButton = document.querySelector(".paste-article-button");
+const textTextarea = document.querySelector(".text-textarea");
 
-pasteTextButton.addEventListener('click', async () => {
+pasteTextButton.addEventListener("click", async () => {
   textTextarea.focus();
   const data = await navigator.clipboard.readText();
   textTextarea.value = data;
@@ -63,43 +63,41 @@ pasteTextButton.addEventListener('click', async () => {
 urlForm.addEventListener("submit", getFactCheck);
 textForm.addEventListener("submit", getFactCheck);
 
-
 // For the fancy toggle
 const st = {};
 
-st.flap = document.querySelector('#flap');
-st.toggle = document.querySelector('.toggle');
+st.flap = document.querySelector("#flap");
+st.toggle = document.querySelector(".toggle");
 
-st.choice1 = document.querySelector('#choice1');
-st.choice2 = document.querySelector('#choice2');
+st.choice1 = document.querySelector("#choice1");
+st.choice2 = document.querySelector("#choice2");
 
-st.flap.addEventListener('transitionend', () => {
-    if (st.choice1.checked) {
-        st.toggle.style.transform = 'rotateY(-15deg)';
-        urlForm.style.display = "none";
-        textForm.style.display = "block";
-        isUrl = false;
-        setTimeout(() => st.toggle.style.transform = '', 50);
-    } else {
-        st.toggle.style.transform = 'rotateY(15deg)';
-        urlForm.style.display = "block";
-        textForm.style.display = "none";
-        isUrl = true;
-        setTimeout(() => st.toggle.style.transform = '', 50);
-    }
-})
-
-st.clickHandler = (e) => {
-
-    if (e.target.tagName === 'LABEL') {
-        setTimeout(() => {
-            st.flap.children[0].textContent = e.target.textContent;
-        }, 50);
-    }
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-    st.flap.children[0].textContent = st.choice2.nextElementSibling.textContent;
+st.flap.addEventListener("transitionend", () => {
+  if (st.choice1.checked) {
+    st.toggle.style.transform = "rotateY(-15deg)";
+    urlForm.style.display = "none";
+    textForm.style.display = "block";
+    isUrl = false;
+    setTimeout(() => (st.toggle.style.transform = ""), 50);
+  } else {
+    st.toggle.style.transform = "rotateY(15deg)";
+    urlForm.style.display = "block";
+    textForm.style.display = "none";
+    isUrl = true;
+    setTimeout(() => (st.toggle.style.transform = ""), 50);
+  }
 });
 
-document.addEventListener('click', (e) => st.clickHandler(e));
+st.clickHandler = (e) => {
+  if (e.target.tagName === "LABEL") {
+    setTimeout(() => {
+      st.flap.children[0].textContent = e.target.textContent;
+    }, 50);
+  }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  st.flap.children[0].textContent = st.choice2.nextElementSibling.textContent;
+});
+
+document.addEventListener("click", (e) => st.clickHandler(e));
--- server.js
+++ server.js
@@ -19,8 +19,8 @@ app.use(express.json());
 
 const data = {
   prompt:
-  // "The Union possessed many features unique among contemporary states. Its political system was characterized by strict checks upon monarchical power. These checks were enacted by a legislature (sejm) controlled by the nobility (szlachta). This idiosyncratic system was a precursor to modern concepts of democracy. Although the two component states of the Commonwealth were formally equal, Poland was the dominant partner in the union. Identify the facts and seperate them with newlines.",
-  "The Poké Ball is a type of Poké Ball introduced in Generation I. It is the most basic form of Poké Ball, an item used to catch a wild Pokémon. The eponymous Poké Ball is the most ubiquitous kind of Poké Ball across the entire Pokémon franchise. It is frequently used to represent the Pokémon series as a whole, such as in the Pokémon series' icon in the Super Smash Bros. series. Identify the facts and seperate them with newlines.",
+    // "The Union possessed many features unique among contemporary states. Its political system was characterized by strict checks upon monarchical power. These checks were enacted by a legislature (sejm) controlled by the nobility (szlachta). This idiosyncratic system was a precursor to modern concepts of democracy. Although the two component states of the Commonwealth were formally equal, Poland was the dominant partner in the union. Identify the facts and seperate them with newlines.",
+    "The Poké Ball is a type of Poké Ball introduced in Generation I. It is the most basic form of Poké Ball, an item used to catch a wild Pokémon. The eponymous Poké Ball is the most ubiquitous kind of Poké Ball across the entire Pokémon franchise. It is frequently used to represent the Pokémon series as a whole, such as in the Pokémon series' icon in the Super Smash Bros. series. Identify the facts and seperate them with newlines.",
   temperature: 0.5,
   max_tokens: 64,
   top_p: 1.0,
@@ -48,38 +48,46 @@ const textAnalysis = async () => {
   }
 };
 
-app.post('/factcheck', async (req, res) => {
-    try {
-      const prompt = req.body.prompt;
-      var response;
-      textAnalysis().then((res) => {
-        response = res;
-        var responseText = response.choices[0].text;
+app.post("/factchecktext", async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+    var response;
+    textAnalysis().then((res) => {
+      response = res;
+      var responseText = response.choices[0].text;
 
-        let factList = responseText.split("\n");
-        //console.log(factList)
+      let factList = responseText.split("\n");
+      console.log(factList);
 
-        const filteredFacts = factList.filter(isFact)
-        console.log("FACT LIST")
-        console.log(filteredFacts)
+      const filteredFacts = factList.filter(isFact);
+      console.log("FACT LIST");
+      console.log(filteredFacts);
 
-        var formattedResponse = data.prompt;
-        filteredFacts.forEach(element => {
-          formattedResponse = formattedResponse.replace(element, "<span style=\"color:red\">" + element + "</span>")
-        });
-        formattedResponse = formattedResponse.replace(" Identify the facts and seperate them with newlines.", "")
-
-        console.log("FORMATTED RESPONSE")
-        console.log(formattedResponse)
+      var formattedResponse = data.prompt;
+      filteredFacts.forEach((element) => {
+        formattedResponse = formattedResponse.replace(
+          element,
+          '<span style="color:red">' + element + "</span>"
+        );
       });
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Something went wrong');
-    }
+      formattedResponse = formattedResponse.replace(
+        " Identify the facts and seperate them with newlines.",
+        ""
+      );
+
+      console.log("FORMATTED RESPONSE");
+      console.log(formattedResponse);
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Something went wrong");
+  }
 });
 
 function isFact(fact) {
   return fact != "";
 }
 
-app.listen(port, () => { console.log(`Avalible on http://localhost:${port}`) });
+app.listen(port, () => {
+  console.log(`Avalible on http://localhost:${port}`);
+});
